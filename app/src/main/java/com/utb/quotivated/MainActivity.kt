e2e96c76a4837dcd0592b45c60152423f5ff949a
SimonSlovak <slovak.sim@seznam.cz>
2023-12-13 03:35:02 +0100

    main activity styles and refactoring

--- app/src/main/java/com/utb/quotivated/MainActivity.kt
+++ app/src/main/java/com/utb/quotivated/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.utb.quotivated
 
-import android.graphics.ColorSpace.Rgb
 import android.os.Bundle
-import android.text.Layout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -14,29 +11,33 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -50,7 +51,6 @@ class MainActivity : ComponentActivity() {
             QuotivatedTheme {
                 val navController = rememberNavController()
 
-                // Set up NavHost with different composable destinations
                 NavHost(navController = navController, startDestination = "main") {
                     composable("main") { MainScreen(navController) }
                     composable("second") { SecondScreen(navController) }
@@ -60,59 +60,93 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
 @Composable
-fun LeftRoundedTriangleButton() {
+fun RoundedBox(boxModifier: Modifier) {
     Box(
         modifier = Modifier
-            .size(130.dp, 40.dp)
-            .border(1.dp, Color.Black, shape = RoundedCornerShape(20.dp, 10.dp, 10.dp, 20.dp))
-            .clip(RoundedCornerShape(20.dp, 10.dp, 10.dp, 20.dp))
-            .background(
-                color = Color.Cyan,
-            )
-    ) {
-        Text(
-            text = "Statistics",
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(10.dp),
-            textAlign = TextAlign.Center
-        )
-    }
-}
-
-@Composable
-fun RoundedBox() {
-    Box(
-        modifier = Modifier
-            .size(40.dp, 40.dp)
-            .border(1.dp, Color.Black, shape = RoundedCornerShape(10.dp))
-            .clip(RoundedCornerShape(10.dp))
-            .background(
-                color = Color.Cyan,
-            )
-    )
-}
-
-@Composable
-fun RightRoundedTriangleButton() {
-    Box(
-        modifier = Modifier
-            .size(130.dp, 40.dp)
-            .border(1.dp, Color.Black, shape = RoundedCornerShape(10.dp, 20.dp, 20.dp, 10.dp))
-            .clip(RoundedCornerShape(10.dp, 20.dp, 20.dp, 10.dp))
+            .size(30.dp, 30.dp)
+            .then(boxModifier)
+            .border(1.5.dp, Color.Black, shape = RoundedCornerShape(5.dp))
+            .clip(RoundedCornerShape(5.dp))
             .background(
                 color = Color.Cyan,
             ),
         contentAlignment = Alignment.Center
+    )
+    {
+        Image(
+            painter = painterResource(id = R.drawable.home),
+            contentDescription = "Home",
+            modifier = Modifier
+                .fillMaxSize(0.6f)
+        )
+    }
+}
+@Composable
+fun CustomNavButton(
+    text: String,
+    shape: RoundedCornerShape,
+    rotation: Float,
+    textRotation: Float,
+    buttonModifier: Modifier
+) {
+    Box(
+        modifier = Modifier
+            .height(30.dp)
+            .fillMaxWidth()
+            .then(buttonModifier)
+            .border(
+                1.5.dp,
+                Color.Black,
+                shape = shape
+            )
+            .clip(shape)
+            .drawWithContent {
+                drawContent()
+            }
+            .graphicsLayer(
+                rotationY = rotation
+            )
+            .background(
+                brush = Brush.horizontalGradient(
+                    colors = listOf(Color.DarkGray, Color.Cyan),
+                    startX = 0f,
+                    endX = 80f
+                )
+            )
     ) {
         Text(
-            text = "Favorites",
+            text = text,
             modifier = Modifier
                 .fillMaxSize()
-                .padding(10.dp),
-            textAlign = TextAlign.Center
+                .padding(7.dp)
+                .graphicsLayer(
+                    rotationY = textRotation
+                ),
+            textAlign = TextAlign.Center,
+            fontSize = 12.sp
+        )
+    }
+}
+
+
+@Composable
+fun CustomBaseButton(text: String, maxWidth: Float) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(maxWidth)
+            .height(30.dp)
+            .border(1.5.dp, Color.Black, shape = RoundedCornerShape(20.dp, 20.dp, 20.dp, 20.dp))
+            .clip(RoundedCornerShape(15.dp))
+            .background(color = Color.Cyan)
+    ) {
+        Text(
+            text = text,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(7.dp),
+            textAlign = TextAlign.Center,
+            fontSize = 12.sp
         )
     }
 }
@@ -120,51 +154,88 @@ fun RightRoundedTriangleButton() {
 @Composable
 fun MainScreen(navController: NavHostController) {
     Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
+        modifier = Modifier
+            .fillMaxSize(),
+        color = Color.Gray
     ) {
-        Column(
+        Box(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            contentAlignment = Alignment.BottomCenter
         ) {
-            // Rest of the content
-
-            Spacer(modifier = Modifier.weight(1f)) // Flexible spacer
-
-            // Box composable to overlay Row with Square
-            Box(
-                modifier = Modifier.fillMaxWidth(),
-                contentAlignment = Alignment.Center
-            ) {
-                // Row layout for buttons
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
+            Column {
+                Row (
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(15.dp, 20.dp, 15.dp, 10.dp),
                     horizontalArrangement = Arrangement.SpaceEvenly,
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    LeftRoundedTriangleButton()
-                    // Button to navigate to SecondScreen
-//                    Button(
-//                        onClick = { navController.navigate("second") },
-//                        modifier = Modifier.width(125.dp)
-//                    ) {
-//                        Text("Stats")
-//                    }
+                    Image(
+                        painter = painterResource(id = R.drawable.earth),
+                        contentDescription = "Earth",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .aspectRatio(1f)
+                            .border(1.5.dp, Color.Black, shape = RoundedCornerShape(15.dp))
+                            .clip(RoundedCornerShape(15.dp))
+                    )
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(15.dp, 20.dp, 15.dp, 0.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    CustomBaseButton("Generate image", 0.45f)
+                    CustomBaseButton("Take photo", 0.90f)
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(15.dp, 20.dp, 15.dp, 0.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    CustomBaseButton("Generate quote", 1f)
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(15.dp, 30.dp, 15.dp, 20.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    CustomNavButton(
+                        text = "Statistics",
+                        shape = RoundedCornerShape(15.dp, 5.dp, 5.dp, 15.dp),
+                        rotation = 0f,
+                        textRotation = 0f,
+                        buttonModifier = Modifier
+                            .weight(0.35f)
+                    )
 
-                    // Square shape (background with primary color)
-                    RoundedBox()
+                    Spacer(modifier = Modifier
+                            .weight(0.05f))
 
-                    RightRoundedTriangleButton()
-                    // Button to navigate to ThirdScreen
-//                    Button(
-//                        onClick = { navController.navigate("third") },
-//                        modifier = Modifier.width(125.dp)
-//                    ) {
-//                        Text("Favorites")
-//                    }
+                    RoundedBox(
+                        boxModifier = Modifier
+                            .weight(0.2f)
+                    )
+
+                    Spacer(modifier = Modifier
+                        .weight(0.05f))
+
+                    CustomNavButton(
+                        text = "Favorites",
+                        shape = RoundedCornerShape(5.dp, 15.dp, 15.dp, 5.dp),
+                        rotation = 180f,
+                        textRotation = 180f,
+                        buttonModifier = Modifier
+                            .weight(0.35f)
+                    )
                 }
             }
         }
@@ -173,11 +244,10 @@ fun MainScreen(navController: NavHostController) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainPreview() {
     QuotivatedTheme {
         val navController = rememberNavController()
 
-        // Call the MainScreen composable
         MainScreen(navController)
     }
 }
